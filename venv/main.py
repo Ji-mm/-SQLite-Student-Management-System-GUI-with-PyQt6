3bea08de595e7f55b7af1945e7404f120a53e0b9
Atudo <jimpirate49@gmail.com>
2023-02-15 16:43:10 +0300

    populate the table with data

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, \
-    QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+    QLabel, QWidget, QGridLayout, QLineEdit, \
+    QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
-
+import sqlite3
 import sys
 
 
@@ -26,11 +27,28 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id #", "Student Name", "Course", "Contacts"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
+        # Get data from database
+    def load_data(self):
+        # Connect to database
+        connection = sqlite3.connect("database.db")
+        # Get the table
+        results = connection.execute("SELECT * FROM students")
+        # Reset the table
+        self.table.setRowCount(0)
+        # Iterate through the tables to extract data
+        for row_number, row_data in enumerate(results):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        # Close connection
+        connection.close()
 
 
 app = QApplication(sys.argv)
-average_speed_calculator = MainWindow()
-average_speed_calculator.show()
+database = MainWindow()
+database.show()
+database.load_data()
 sys.exit(app.exec())
