45020e5fe2de4828b9dd226e48c53c3689c550af
Atudo <jimpirate49@gmail.com>
2023-02-15 16:09:57 +0300

    add table structure

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, \
-    QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+    QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
 from PyQt6.QtGui import QAction
 
 import sys
@@ -21,6 +21,13 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         about_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+        # Add a table structure
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id #", "Student Name", "Course", "Contacts"))
+        self.setCentralWidget(self.table)
+
 
 
 app = QApplication(sys.argv)
