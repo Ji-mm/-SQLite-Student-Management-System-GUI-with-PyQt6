0374a43d0939d058ae24b1544999b1929019f697
Atudo <jimpirate49@gmail.com>
2023-02-15 15:59:14 +0300

    added menu and submenu bars

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, \
     QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtGui import QAction
 
 import sys
 
@@ -8,6 +9,18 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+
+        # Add a menu bar
+        file_menu_item = self.menuBar().addMenu("&File")
+        about_menu_item = self.menuBar().addMenu("&Help")
+
+        # Add submenu for menu bar
+        file_menu_action = QAction("Add Student", self)
+        file_menu_item.addAction(file_menu_action)
+
+        about_action = QAction("About", self)
+        about_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
 
 
 app = QApplication(sys.argv)
